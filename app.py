85832dd72631619fc875dcb6ede4e8db1d0e5e5e
Vivek <vivekumachagi56@gmail.com>
2023-08-31 15:25:46 +0530

    db connected

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:codilar@localhost/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+@app.route('/insert')
+def insertData():
+    my_data = Data(username="viveksdf", email="vivsadaek@gmail.com")
+    db.session.add(my_data)
+    db.session.commit()
 
 @app.route('/')
 def hello_world():
     return render_template('home.html')
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
