fda3cd940ce6c5bc77ca6506e65683d971f60c59
Vivek <vivekumachagi56@gmail.com>
2023-09-01 10:16:56 +0530

    2nd day

--- app.py
+++ app.py
@@ -6,11 +6,18 @@ app.secret_key = "Secret Key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:codilar@localhost/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.app_context().push()
 
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    grade = db.Column 
+    section = db.Column  
 
 @app.route('/insert')
 def insertData():
@@ -23,4 +30,5 @@ def hello_world():
     return render_template('home.html')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
