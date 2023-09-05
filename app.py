f790c0cce27750820fd10c71425b9f85a5fa95dd
Vivek <vivekumachagi56@gmail.com>
2023-09-05 12:14:53 +0530

    login working

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "Secret Key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:codilar@localhost/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(
+    minutes=30)  # Set the session timeout to 30 minutes
 db = SQLAlchemy(app)
 app.app_context().push()
+
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # This function is called to load a user based on the user_id stored in the session
+    return Students.query.get(int(user_id))
 
 
 class Data(db.Model):
@@ -16,7 +29,7 @@ class Data(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 
 
-class Students(db.Model):
+class Students(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(30))
@@ -24,46 +37,80 @@ class Students(db.Model):
     password = db.Column(db.String(60))
 
 
+def getUser(email):  # verifying Email exist in database
+    return Students.query.filter_by(email=email).first()
+
+
+def is_active(self):
+    return True  # You can customize this based on your requirements
+
+
+def is_authenticated(self):
+    return True  # You can customize this based on your requirements
+
+
 @app.route('/signup')
-def render_signup():
+def render_signup():  # Renders REGISTER page
     return render_template('signup.html')
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/')  # Renders HOME page
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/register', methods=['POST'])  # registering the user
 def insert():
     if request.method == 'POST':
         username = request.form['name']
-        formemail = request.form['email']   
+        formemail = request.form['email']
         grade = request.form['grade']
         password = request.form['password']
         confirmpassword = request.form['confirmpassword']
-        user = Students.query.filter_by(email=formemail).first()
-        if  user:
+        user = getUser(formemail)
+        if user:
             flash('account already exist please login!', 'error')
             return redirect(url_for('home_page'))
-        elif  password != confirmpassword:
-             flash('Invalid password!', 'error')
-             return redirect(url_for('home_page'))
+        elif password != confirmpassword:
+            flash('Invalid password!', 'error')
+            return redirect(url_for('home_page'))
 
         else:
             my_data = Students(name=username, email=formemail,
-                           grade=grade, password=password)
+                               grade=grade, password=password)
             db.session.add(my_data)
             db.session.commit()
             flash('Data inserted successful!', 'success')
             return redirect(url_for('home_page'))
 
 
-@app.route('/login')
-def insertData():
-    my_data = Data(username="viveksdf", email="vivsadaek@gmail.com")
-    db.session.add(my_data)
-    db.session.commit()
+@app.route('/login')  # Renders LOGIN page
+def login_page():
+    return render_template('login.html')
 
 
-@app.route('/')
-def home_page():
-    return render_template('home.html')
+@app.route('/postlogin', methods=['POST'])  # verify user for LOGIN process
+def postLogin():
+    if (request.method == 'POST'):
+        email = request.form['email']
+        password = request.form['password']
+        user = getUser(email)
+        if (user and user.password == password):
+            flash('Login successful!', 'success')
+            login_user(user)
+            return login_page()
+        else:
+            flash('Invalid Details!', 'error')
+            return login_page()
+    else:
+        return render_template('home.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('login_page'))
 
 
 if __name__ == '__main__':
--- static/js/header.js
+++ static/js/header.js
@@ -3,8 +3,15 @@ $(document).ready(function () {
     $("#header-signup").click(function () {
         redirectToSignup(); // Call the redirectToSignup function
     });
+    // Attach a click event handler to the "LOGIN" button
+    $("#header-login").click(function () {
+        redirectToLogin(); // Call the redirectToLogin function
+    });
 
 });
 function redirectToSignup() {
     window.location.href = '/signup'; // Redirect to the signup route
+}
+function redirectToLogin(){
+    window.location.href = '/login'; // Redirect to the login route
 }
