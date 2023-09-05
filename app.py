27a70e659fec02f00e8002c750ab8014aa30f9bc
Vivek <vivekumachagi56@gmail.com>
2023-09-05 10:10:29 +0530

    3rd

--- app.py
+++ app.py
@@ -9,47 +9,62 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
 
+
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+
 class Students(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    grade = db.Column 
-    section = db.Column  
+    email = db.Column(db.String(30))
+    grade = db.Column(db.Integer)
+    password = db.Column(db.String(60))
+
 
 @app.route('/signup')
 def render_signup():
     return render_template('signup.html')
 
-@app.route('/register', methods = ['POST'])
+
+@app.route('/register', methods=['POST'])
 def insert():
     if request.method == 'POST':
         username = request.form['name']
-        email = request.form['email']
-        user = Data.query.filter_by(username=username).first()
-        if user and user.email == email:
-            login_user(user)
-            flash('Login successful!', 'success')
-            return redirect(url_for('hello_world'))
+        formemail = request.form['email']   
+        grade = request.form['grade']
+        password = request.form['password']
+        confirmpassword = request.form['confirmpassword']
+        user = Students.query.filter_by(email=formemail).first()
+        if  user:
+            flash('account already exist please login!', 'error')
+            return redirect(url_for('home_page'))
+        elif  password != confirmpassword:
+             flash('Invalid password!', 'error')
+             return redirect(url_for('home_page'))
+
         else:
-            my_data = Data(username=username, email=email)
+            my_data = Students(name=username, email=formemail,
+                           grade=grade, password=password)
             db.session.add(my_data)
             db.session.commit()
             flash('Data inserted successful!', 'success')
+            return redirect(url_for('home_page'))
 
-    return redirect(url_for('hello_world'))
+
 @app.route('/login')
 def insertData():
     my_data = Data(username="viveksdf", email="vivsadaek@gmail.com")
     db.session.add(my_data)
     db.session.commit()
 
+
 @app.route('/')
-def hello_world():
+def home_page():
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     db.create_all()
